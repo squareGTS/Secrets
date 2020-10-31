76a5809f764a7ad298a530939ae495a50b0cf3a8
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-10-31 12:34:12 +0300

    add passport

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-require("dotenv").config();
+//require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 //const encrypt = require("mongoose-encryption");
 //const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 
 const app = express();
 
@@ -18,20 +22,37 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:32772/userDB", {
   useNewUrlParser: true
 });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 //Secret String Insted of Two Keys
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -46,40 +67,95 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+//Update the code
+
 app.post("/register", function(req, res) {
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-      email: req.body.username,
-      password: hash //md5(req.body.password)
-    });
-
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      })
+    };
   });
 });
 
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = md5(req.body.password);
 
-  User.findOne({email: username}, function(err, foundUser) {
+
+
+
+// bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//   const newUser = new User({
+//     email: req.body.username,
+//     password: hash //md5(req.body.password)
+//   });
+//
+//   newUser.save(function(err) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.render("secrets");
+//     }
+//   });
+// });
+
+
+app.post("/login", function(req, res) {
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
     }
   });
 });
+
+  // const username = req.body.username;
+  // const password = req.body.password; //md5(req.body.password);
+  //
+  // User.findOne({
+  //     email: username
+  //   }, function(err, foundUser) {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       if (foundUser) {
+  //         bcrypt.compare(password, foundUser.password, function(err, result) {
+  //           if (result === true) {
+  //             res.render("secrets");
+  //           }
+  //         });
+  //         //if (foundUser.password === password) {
+  //       }
+  //     }
+  //   }
+  // });
+
 
 
 
